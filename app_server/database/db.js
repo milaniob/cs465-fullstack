8bc08217e282bd989d1d7a4d0d22703df9949ab5
Oliver <oliver.milani@snhu.edu>
2022-10-02 19:55:10 -0400

    Convert travel template from static data to dynamic JSON

---
+++ app_server/database/db.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const host = process.env.DB_HOST || '127.0.0.1'
+const dbURL = `mongodb://${host}/travlr`;
+const readLine = require('readline');
+
+
+//avoid 'current Server Discovery and Mintoring engine is deprecated'
+mongoose.set('useUnifiedTopology', true);
+
+if (process.env.NODE_ENV === 'production') {
+  dbURL = process.env.DB_HOST || process.env.MONGODB_URI;
+}
+
+const connect = () => {
+  setTimeout(() => mongoose.connect(dbURL, { 
+        useNewUrlParser: true, 
+        useCreateIndex: true, 
+        }), 1000);
+}
+
+mongoose.connection.on('connected', () => {
+});
+
+mongoose.connection.on('error', err => {
+});
+
+mongoose.connection.on('disconnected', () => {
+});
+
+if (process.platform === 'win32') {
+  };
+
+const gracefulShutdown = (msg, callback) => {
+  
+};
+
+
+// for nodemon restarts
+process.once('SIGUSR2', () => {
+  });
+
+// for app termination
+process.on('SIGINT', () => {
+});
+
+// for heroku app termination
+process.on('SIGTERM', () => {
+});
+
+connect();
+
+// bring in the Mongoose schema
+require('./travlr');
