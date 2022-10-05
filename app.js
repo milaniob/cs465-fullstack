e35f09cc2652c75a5cbd7f32bba014eb314c0ab8
Oliver <oliver.milani@snhu.edu>
2022-10-04 20:23:52 -0400

    Create new Application API

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const hbs = require('hbs');
+require('./app_api/database/db');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel')
+var travelRouter = require('./app_server/routes/travel');
+var apiRouter = require('./app_api/routes/index');
 
 var app = express();
 
@@ -30,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ app_api/controllers/trips.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const model = mongoose.model('trips');
+
+//Get: /trips - list all the trips
+const tripsList = async(req, res) => {
+    model
+        .find({}) //empty filter will return all the trips
+        .exec((err, trips)=> {
+            if(!trips){
+                return res
+                    .status(404)
+                    .json({"message": "trip not found"});
+            } else if (err) {
+                return res 
+                    .status(404)
+                    .json(err);
+            }else{
+                return res
+                    .status(200)
+                    .json(trips)
+            }
+        });     
+};
+
+// GET: /trips/:tripCode - returns a single trip
+const tripsFindCode = async (req, res) => {
+    model
+        .find({ 'code': req.params.tripCode })
+        .exec((err, trip) => {
+            if (!trip) {
+                return res
+                    .status(404)
+                    .json({ "message": "Trip not found" });
+            } else if (err) {
+                return res
+                    .status(404)
+                    .json(err);
+            } else {
+                return res
+                    .status(200)
+                    .json(trip)
+            }
+        });
+};
+
+
+module.exports = {
+    tripsList,
+    tripsFindCode
+};
